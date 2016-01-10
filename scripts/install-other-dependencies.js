97b8b4a367b928ece18e8fd80e3701c7cd76dc88
varun <varun8686@gmail.com>
2016-01-09 20:33:09 -0700

    fixing ci

--- scripts/install-other-dependencies.js
+++ scripts/install-other-dependencies.js
@@ -1,14 +1,1 @@
 
-
-var util = require('./util');
-
-util.exec("npm install",function(error){
-
-    if(error)
-    {
-        console.log(error);
-        process.exit(1);
-    }
-
-    process.exit(0);
-});
