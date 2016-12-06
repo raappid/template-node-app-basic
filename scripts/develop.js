1833740ce56f66e2b2dcf8694c4fa15776f99319
varunreddy <varun8686@gmail.com>
2016-12-05 20:38:11 -0700

    adding a basic server as sample project.

---
+++ scripts/develop.js
@@ -0,0 +1,8 @@
+
+
+require("ts-node/register");
+
+
+//requiring the main file to start the server;
+require("../src/main");
+
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,67 @@
 
 
-import {EventConstants, Actions} from "./service_system/constants";
+import {EventConstants} from "./service_system/constants";
 
 import {myStore, myManager} from "./service_system/index";
-//subscribing to an event
 
-myStore.addListener(EventConstants.MyStore.HI_HELLO,(result)=>{
-    console.log("My Store data updated: " + result)
+//Lets require/import the HTTP module
+import {IncomingMessage,createServer,ServerResponse} from "http";
+
+import {parse as parseURL,Url} from "url"
+
+
+//Lets define a port we want to listen to
+const PORT= process.env.NODE_ENV == "production"? process.env.PORT:3000;
+
+//We need a function which handles requests and send response
+function handleRequest(req:IncomingMessage, res:ServerResponse){
+
+    //subscribing to an event
+    myStore.addListener(EventConstants.MyStore.HI_HELLO,handleHiHelloEvent);
+
+    var urlObject:Url = parseURL(req.url);
+
+    if(urlObject.path == "/")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(`<!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                        </head>
+                        <body>
+                            <a href="/helloworld">Say Hello</a>
+                        </body>
+                    </html>`);
+    }
+    else if(urlObject.path == "/helloworld")
+    {
+        myManager.sayHello();
+    }
+    else
+    {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Sorry Could not find anything :(');
+    }
+
+
+    function handleHiHelloEvent(result:string):void
+    {
+        myStore.removeListener(EventConstants.MyStore.HI_HELLO,handleHiHelloEvent);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(result);
+    }
+
+}
+
+//Create a server
+var server = createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server Started at port", PORT);
 });
 
-//performing an action
-myManager.sayHello();
 
 
 
